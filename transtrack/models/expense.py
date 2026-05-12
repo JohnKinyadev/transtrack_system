ef8ebe3f1051061ac3e1bfc4f955f394dd6b9970
mushkorr <kinyaj25@gmail.com>
2026-05-13 01:38:29 +0300

    feat: add expense model document builder

---
+++ transtrack/models/expense.py
@@ -0,0 +1,14 @@
+from transtrack.models.base import with_timestamps
+
+
+def expense_document(trip_id, vehicle_id, expense_type, amount, logged_by, date):
+    return with_timestamps(
+        {
+            "trip_id": str(trip_id),
+            "vehicle_id": str(vehicle_id),
+            "type": expense_type,
+            "amount": float(amount),
+            "logged_by": str(logged_by),
+            "date": date,
+        }
+    )
