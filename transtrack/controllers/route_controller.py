ad69c7561db9055ea8c54c0ae0ec114b8e1b933d
mushkorr <kinyaj25@gmail.com>
2026-05-13 01:38:39 +0300

    feat: add route controller

---
+++ transtrack/controllers/route_controller.py
@@ -0,0 +1,7 @@
+from transtrack.controllers.base_controller import BaseController
+
+
+class RouteController(BaseController):
+    collection_name = "routes"
+    module_name = "routes"
+    id_prefix = "R"
