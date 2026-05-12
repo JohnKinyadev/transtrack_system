69c9aca1d991df9f688cdc3a7da2500d46655fc6
mushkorr <kinyaj25@gmail.com>
2026-05-13 01:38:36 +0300

    feat: add owner controller

---
+++ transtrack/controllers/owner_controller.py
@@ -0,0 +1,7 @@
+from transtrack.controllers.base_controller import BaseController
+
+
+class OwnerController(BaseController):
+    collection_name = "owners"
+    module_name = "owners"
+    id_prefix = "O"
