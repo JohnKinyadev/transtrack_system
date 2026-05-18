038d56fd758e6e0083597e87b56ef2c43c9b7a30
mushkorr <kinyaj25@gmail.com>
2026-05-18 10:57:36 +0300

    Normalize payout totals with helper

--- transtrack/models/payout.py
+++ transtrack/models/payout.py
@@ -1,4 +1,5 @@
 from transtrack.models.base import with_timestamps
+from transtrack.utils.numbers import to_float
 
 
 def payout_document(owner_id, period, gross_earnings, total_deductions, net_payout, date):
@@ -6,9 +7,9 @@ def payout_document(owner_id, period, gross_earnings, total_deductions, net_payo
         {
             "owner_id": str(owner_id),
             "period": period,
-            "gross_earnings": float(gross_earnings),
-            "total_deductions": float(total_deductions),
-            "net_payout": float(net_payout),
+            "gross_earnings": to_float(gross_earnings),
+            "total_deductions": to_float(total_deductions),
+            "net_payout": to_float(net_payout),
             "date": date,
         }
     )
