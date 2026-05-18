5c5546e7d9538139f8b148ef517137dc89374ea3
mushkorr <kinyaj25@gmail.com>
2026-05-18 10:57:45 +0300

    Add stage manager controller

---
+++ transtrack/controllers/stage_manager_controller.py
@@ -0,0 +1,7 @@
+from transtrack.controllers.base_controller import BaseController
+
+
+class StageManagerController(BaseController):
+    collection_name = "stage_managers"
+    module_name = "stage_managers"
+    id_prefix = "S"
