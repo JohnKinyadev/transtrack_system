947772669c24ed524ae8b8c37f84230564a073d7
mushkorr <kinyaj25@gmail.com>
2026-05-18 10:57:42 +0300

    Assign stage managers to trips

--- transtrack/controllers/trip_controller.py
+++ transtrack/controllers/trip_controller.py
@@ -1,7 +1,8 @@
 from transtrack.config import TRIP_STATUSES
 from transtrack.controllers.base_controller import BaseController
-from transtrack.models.base import to_object_id
 from transtrack.utils.audit import log_action
+from transtrack.utils.relations import reference_query
+from transtrack.utils.session import get_current_user
 
 
 class TripController(BaseController):
@@ -13,17 +14,25 @@ class TripController(BaseController):
         "driver_id": {"collection": "drivers", "label": "Driver ID"},
         "conductor_id": {"collection": "conductors", "label": "Conductor ID"},
         "route_id": {"collection": "routes", "label": "Route ID"},
+        "stage_manager_id": {"collection": "stage_managers", "label": "Stage Manager ID"},
     }
     future_date_fields = {"date": "Trip date"}
 
+    def create(self, data):
+        user = get_current_user() or {}
+        if user.get("role") == "stage_manager" and user.get("linked_id"):
+            data = dict(data)
+            data["stage_manager_id"] = user.get("linked_id")
+        return super().create(data)
+
     def list_for_driver(self, driver_id):
-        return self.list_all({"driver_id": driver_id}, [("date", -1)])
+        return self.list_all(reference_query("driver_id", "drivers", driver_id), [("date", -1)])
 
     def list_for_conductor(self, conductor_id):
-        return self.list_all({"conductor_id": conductor_id}, [("date", -1)])
+        return self.list_all(reference_query("conductor_id", "conductors", conductor_id), [("date", -1)])
 
     def list_for_vehicle(self, vehicle_id):
-        return self.list_all({"vehicle_id": vehicle_id}, [("date", -1)])
+        return self.list_all(reference_query("vehicle_id", "vehicles", vehicle_id), [("date", -1)])
 
     def update_status(self, trip_id, status):
         if status not in TRIP_STATUSES:
